eda7daf0dfb5551df766a60363602b666222cd98
MALPV <marcospalma98@outlook.com>
2019-12-11 20:21:16 -0300

    Se añade README

---
+++ Assets/Scripts/color.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color : MonoBehaviour {
+
+    //Forma uno de cambiar el color
+    //public float r, g, b;
+    public Color colorCubo;
+
+	// Use this for initialization
+	void Start () {
+        //Forma 1 GetComponent<Renderer>().material.color = new Color(r, g, b);
+        GetComponent<Renderer>().material.color = colorCubo;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+
+    public float deltaRotacion = 100f;
+    public float deltaMovimiento = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotar();
+        Movimiento();
+    }
+
+    void Rotar()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(new Vector3(0f, -deltaRotacion, 0f) * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(new Vector3(0f, deltaRotacion, 0f) * Time.deltaTime);
+    }
+
+    void Movimiento()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.back * deltaMovimiento * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.forward * deltaMovimiento * Time.deltaTime);
+    }
+}
+   
